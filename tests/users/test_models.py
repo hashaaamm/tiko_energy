b87903ae86ef6b353bd709addc7e114ea67e12b6
Hashaam Hassan <hashaam.hassan.work@gmail.com>
2024-03-07 18:36:40 +0000

    add custom_user model, customer_user model test, djoser jwt auth with 1 hour lifetime

---
+++ tests/users/test_models.py
@@ -0,0 +1,18 @@
+import pytest
+
+from events.models import CustomUser
+
+@pytest.mark.django_db
+def test_custom_user_model():
+    email="john@gmail.com"
+    password = "sdadfad3sd"
+    user = CustomUser(email=email, password=password)
+    user.save()
+    assert user.email == email
+    assert user.password == password
+    assert not user.is_admin
+    assert not user.is_staff
+
+
+def test_foo():
+    assert "asas" != "asasasas"
